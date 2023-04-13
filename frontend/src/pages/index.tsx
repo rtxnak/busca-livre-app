bd886f37c10136aa4aee51dc3769eb77742cb55a
Rafael Nakashima <rafaelnakashima@rocketmail.com>
2023-04-13 18:02:58 -0300

    feat: created frontend and installed dependencies

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Busca Livre</title>
+      </Head>
+      <main>
+        Page
+      </main>
+    </div>
+  )
+}
