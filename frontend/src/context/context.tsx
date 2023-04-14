bc5981282bcfe5b524b00a5a73565fc0d9c263db
Rafael Nakashima <rafaelnakashima@rocketmail.com>
2023-04-14 01:34:52 -0300

    feat: added Mercado Livre parse function on context

--- frontend/src/context/context.tsx
+++ frontend/src/context/context.tsx
@@ -1,3 +1,5 @@
+import { MercadoLivreCategoriesIds } from "@/helpers/categories";
+import { mercadoLivreGetProductsFromCategoryAndQuery } from "@/services/mercadoLivreAPI";
 import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react";
 
 type contextProps = {
@@ -6,20 +8,39 @@ type contextProps = {
 
 type contextType = {
   webPageSelection: string,
-  setWebPageSelection: Dispatch<SetStateAction<string>>;
+  setWebPageSelection: Dispatch<SetStateAction<string>>,
   categorySelection: string,
-  setCategorySelection: Dispatch<SetStateAction<string>>;
+  setCategorySelection: Dispatch<SetStateAction<string>>,
   queryInput: string,
-  setQueryInput: Dispatch<SetStateAction<string>>;
+  setQueryInput: Dispatch<SetStateAction<string>>,
 }
 
 export const Context = createContext<contextType>({} as contextType);
 
 export const ContextProvider = ({ children }: contextProps) => {
-  
+
   const [webPageSelection, setWebPageSelection] = useState<string>('Todas');
   const [categorySelection, setCategorySelection] = useState<string>('');
   const [queryInput, setQueryInput] = useState<string>('');
+
+  const mercadoLivreResultsParse = async () => {
+    const categoriesArray = Object.entries(MercadoLivreCategoriesIds)
+    const categoryFiltered = categoriesArray.filter((key) => key[0] === categorySelection);
+    const categoryId = categoryFiltered.length > 1 ? categoryFiltered[0][1] : ""
+
+    const { results } = await mercadoLivreGetProductsFromCategoryAndQuery(categoryId, queryInput);
+    const mercadoLivreParsedData = results.map((result: any) => {
+      return (
+        {
+          description: result.title,
+          img: result.thumbnail,
+          price: (result.price).toString(),
+          link: result.permalink
+        }
+      )
+    })
+    return mercadoLivreParsedData;
+  }
 
   return (
     <Context.Provider
